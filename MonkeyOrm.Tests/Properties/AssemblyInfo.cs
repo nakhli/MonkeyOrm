1128194489ada07c22e8f6c85752a620d2f1e2eb
nakhli <chaker.nakhli@javageneration.com>
2013-02-01 11:12:45 +0100

    update test project assembly info + remove unused file

--- MonkeyOrm.Tests/Properties/AssemblyInfo.cs
+++ MonkeyOrm.Tests/Properties/AssemblyInfo.cs
@@ -1,16 +1,28 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// <copyright file="AssemblyInfo.cs" company="Sinbadsoft">
+// Copyright (c) Chaker Nakhli 2013
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by 
+// applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+// </copyright>
+// <author>Chaker Nakhli</author>
+// <email>chaker.nakhli@sinbadsoft.com</email>
+// <date>2013/02/01</date>
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sinbadsoft.Lib.Data.Tests")]
+[assembly: AssemblyTitle("MonkeyOrm.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sinbadsoft")]
 [assembly: AssemblyProduct("Sinbadsoft.Lib.Data.Tests")]
-[assembly: AssemblyCopyright("Copyright © Sinbadsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Sinbadsoft 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
